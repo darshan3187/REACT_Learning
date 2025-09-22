b497f95dbcdbdcee57d353ebe30763cab78903b0
darshan3187 <darshanrajgor73@gmail.com>
2025-09-22 15:09:09 +0530

    complete

--- Mega_Project/src/App.jsx
+++ Mega_Project/src/App.jsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-import { useDispatch } from 'react-redux';
-import authService from './appWritter/auth';
-import { Header, Footer } from './components';
-import {login , logout } from './store/authSlice';
-import { Outlet } from 'react-router-dom';
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     authService.getCurrentUser()
-      .then((user) => {
-        if (user) {
-          dispatch(login({user}));
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
         }
-          dispatch(logout());
       })
-      .finally(() => setLoading(false));
-  }, []);
+      .finally(() => setLoading(false))
+  }, [])
 
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between">
+    <div className='min-h-screen flex flex-wrap content-between text-white bg-black-700'>
       <div className='w-full block'>
         <Header />
         <main>
-          {/* <Outlet /> */}
+          TODO:  <Outlet />
         </main>
         <Footer />
       </div>
--- Mega_Project/src/appwrite/config.js
+++ Mega_Project/src/appwrite/config.js
@@ -1,20 +1,20 @@
-import { Client, ID , Databases, Storage, Query } from "appwrite";
-import conf from '../conf/conf';
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service{
+export class Service {
     client = new Client();
     databases;
     bucket;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
-        this.databases = new Databases(this.client)
+        this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title , slug , content , featuredImage, status, userId}){
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
@@ -29,11 +29,11 @@ export class Service{
                 }
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
 
-    async updatePost(slug, { title, content, featuredImage, status}){
+    async updatePost(slug, { title, content, featuredImage, status }) {
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
@@ -43,92 +43,95 @@ export class Service{
                     title,
                     content,
                     featuredImage,
-                    status
+                    status,
+
                 }
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
-    async deletePost(slug){
+    async deletePost(slug) {
         try {
             await this.databases.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
+
             )
-            return true;
+            return true
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
-            return false;
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
         }
     }
 
-    async getPost(slug){
+    async getPost(slug) {
         try {
             return await this.databases.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
+
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
-            return false;
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
         }
     }
 
-    async listPosts(queries = [Query.equal("status" , "active")]){
+    async getPosts(queries = [Query.equal("status", "active")]) {
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
+
+
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
-            return false;
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
         }
     }
 
-    //file upload Service
-    async uploadFile(file){
+    // file upload service
+
+    async uploadFile(file) {
         try {
             return await this.bucket.createFile(
                 conf.appwriteBucketId,
                 ID.unique(),
-                file,
+                file
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
-            return false;
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
         }
     }
 
-    //file delete Service
-    async deleteFile(fileId){
+    async deleteFile(fileId) {
         try {
             await this.bucket.deleteFile(
                 conf.appwriteBucketId,
-                fileId,
+                fileId
             )
-            return true;
+            return true
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
-            return false;
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
         }
     }
 
-    getFilePpreview(fileId){
+    getFilePreview(fileId) {
         return this.bucket.getFilePreview(
             conf.appwriteBucketId,
-            fileId,
+            fileId
         )
     }
-
 }
 
 
-const service = new Service();
-
-export default service;
+const service = new Service()
+export default service
